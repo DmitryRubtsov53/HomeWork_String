3ca3571bd13b05c478acf5ad1730039cbd6f38a2
DmitryRubtsov53 <2578390@mail.ru>
2022-09-19 20:41:47 +0500

    Решены 3 задачи основной части ДЗ Строки

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание Урока Строки ****************************************************");
         System.out.println();
+        System.out.println("Задача 1. Напечатать ФИО сотрудника");
 
+        String fistName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + fistName + " " + middleName;
+
+        System.out.println(fullName);
+
+        System.out.println();
+        System.out.println("Задача 2. Напечатать ФИО сотрудника в верхнем регистре");
+
+        String UpName = fullName.toUpperCase();
+        System.out.println(UpName);
+
+        System.out.println();
+        System.out.println("Задача 3. Заменить в ФИО сотрудника буквы ё на буквы е.");
+
+        String newName = "Иванов Семён Семёнович";
+        String new2Name = newName.replace ('ё', 'е');
+        System.out.println(new2Name);
 
 
 

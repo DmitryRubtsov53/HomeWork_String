64128b616cfb4d2190b3eb0c15e01fda2362043b
DmitryRubtsov53 <2578390@mail.ru>
2022-09-19 18:56:56 +0500

    Начинаем делать ДЗ Строки

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание Урока Строки ****************************************************");
+        System.out.println();
+
+
+
+
+    }
+}
